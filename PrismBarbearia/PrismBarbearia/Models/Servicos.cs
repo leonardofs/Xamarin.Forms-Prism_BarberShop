02f066003ef24056262c37b4b5cf1b507e62077e
Leonardo Flávio Santos <leonardoflavios@gmail.com>
2017-06-15 20:02:35 -0300

    --

--- PrismBarbearia/PrismBarbearia/Models/Servicos.cs
+++ PrismBarbearia/PrismBarbearia/Models/Servicos.cs
@@ -8,5 +8,9 @@ namespace PrismBarbearia.Models
 {
     class Servicos
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }     
+        public string Image { get; set; }
     }
 }
