999ad69d3a5f31f436ef46860fec4018d53c3320
Leonardo Flávio Santos <leonardoflavios@gmail.com>
2017-06-18 11:16:11 -0300

    retirada da masterpage, menu agora fica dentro da masterdetailpage, codigo rodando normalmente agora

--- PrismBarbearia/PrismBarbearia/App.xaml.cs
+++ PrismBarbearia/PrismBarbearia/App.xaml.cs
@@ -6,25 +6,28 @@ namespace PrismBarbearia
 {
     public partial class App : PrismApplication
     {
-        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+            InitializeComponent();
+        }
 
         protected override void OnInitialized()
         {
-            InitializeComponent();
+           
 
-            NavigationService.NavigateAsync("MenuPage/MasterPage/MainPage/");
+            NavigationService.NavigateAsync("Navigation/MenuPage/MainPage");
         }
 
         protected override void RegisterTypes()
         {
-            //Container.RegisterTypeForNavigation<NavigationPage>("Navigation");
+            Container.RegisterTypeForNavigation<NavigationPage>("Navigation");
             Container.RegisterTypeForNavigation<MainPage>();
             Container.RegisterTypeForNavigation<MenuPage>();
             Container.RegisterTypeForNavigation<MainPage>();
             Container.RegisterTypeForNavigation<AboutPage>();
             Container.RegisterTypeForNavigation<ContactPage>();
             Container.RegisterTypeForNavigation<SchedulesPage>();
-            Container.RegisterTypeForNavigation<MasterPage>();
+            
         }
     }
 }
