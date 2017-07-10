7a243d86918dcc0bb375f633b7ce81ecc1e2de15
joão marcoos <jmarcossferreira@hotmail.com>
2017-07-09 21:50:24 -0300

    Excluindo sabado dos dias

--- PrismBarbearia/PrismBarbearia/ViewModels/DaysPageViewModel.cs
+++ PrismBarbearia/PrismBarbearia/ViewModels/DaysPageViewModel.cs
@@ -29,7 +29,7 @@ namespace PrismBarbearia.ViewModels
                 DateTime date = DateTime.Today.AddDays(i);
                 BarberDay day = new BarberDay();
                 day.Date = date.ToString("dd-MM-yyyy");
-                if (date.DayOfWeek == 0)
+                if (date.DayOfWeek == 0 || date.DayOfWeek == DayOfWeek.Saturday)
                     continue;
                 else
                     Days.Add(day);
