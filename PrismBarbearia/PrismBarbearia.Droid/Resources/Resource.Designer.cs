e099a2888a21a6f2389790cb38006641028cf368
joão marcoos <jmarcossferreira@hotmail.com>
2017-07-11 14:44:15 -0300

    Implementação aboutviewModel

--- PrismBarbearia/PrismBarbearia.Droid/Resources/Resource.Designer.cs
+++ PrismBarbearia/PrismBarbearia.Droid/Resources/Resource.Designer.cs
@@ -3972,26 +3972,8 @@ namespace PrismBarbearia.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_1 = 2130837820;
-			
-			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_2 = 2130837821;
-			
-			// aapt resource value: 0x7f02013e
-			public const int avd_hide_password_3 = 2130837822;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
-			
-			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_1 = 2130837823;
-			
-			// aapt resource value: 0x7f020140
-			public const int avd_show_password_2 = 2130837824;
-			
-			// aapt resource value: 0x7f020141
-			public const int avd_show_password_3 = 2130837825;
 			
 			// aapt resource value: 0x7f020055
 			public const int banner = 2130837589;
--- PrismBarbearia/PrismBarbearia/ViewModels/AboutTabPageViewModel.cs
+++ PrismBarbearia/PrismBarbearia/ViewModels/AboutTabPageViewModel.cs
@@ -5,9 +5,16 @@ namespace PrismBarbearia.ViewModels
 {
     public class AboutTabPageViewModel : BaseViewModel
     {
+        public string ConceptText { get; set; }
         public AboutTabPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService) : base(navigationService, pageDialogService)
         {
             Title = "A BARBEARIA";
+            ConceptText = "Toalha quente, navalha e cordialidade, a barbearia Eight Ball tem uma inovadora proposta em estética," +
+                " exclusivamente para o público masculino, com estrutura ampla, moderna e bem decorada; ambiente acolhedor," +
+                " climatizado e muito agradável, atentando para o retorno da clássica elegância masculina e os cuidados cada vez maiores com a aparência. " +
+                "A barbearia traz serviços como os classicos barba, cabelo e bigode, a tratamentos modernos e tinturas, " +
+                "Aproveite nosso ambiente super agradável com bar e mesa de sinuca, WIFI, programação esportiva, revistas especializadas, café espresso, " +
+                "cervejas especiais e uma linha de produtos formuladas para as necessidades específicas do homem.";
         }
     }
 }
